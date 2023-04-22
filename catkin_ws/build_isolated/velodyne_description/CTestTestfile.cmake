d11f8d76b6566b86189289e87b753b0518486ae6
Jash Shah <jashshah.999@gmail.com>
2023-04-22 19:43:52 -0400

    Change robot in gazebosim to Stretch

---
+++ catkin_ws/build_isolated/velodyne_description/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jashshah999/vlr_project/VLR-Project/catkin_ws/src/velodyne_simulator/velodyne_description
+# Build directory: /home/jashshah999/vlr_project/VLR-Project/catkin_ws/build_isolated/velodyne_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ catkin_ws/devel_isolated/setup.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# generated from catkin.builder Python module
+
+. "/home/jashshah999/vlr_project/VLR-Project/catkin_ws/devel_isolated/velodyne_simulator/setup.bash"
